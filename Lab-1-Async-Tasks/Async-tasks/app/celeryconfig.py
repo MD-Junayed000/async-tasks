7d5c38501fe37ee84e9c34a0129f680ab23aad3f
Muhammad Junayed <u2008023@student.cuet.ac.bd>
2025-06-04 03:15:13 +0600

    Update celeryconfig.py

--- Lab-1-Async-Tasks/Async-tasks/app/celeryconfig.py
+++ Lab-1-Async-Tasks/Async-tasks/app/celeryconfig.py
@@ -1,9 +1,9 @@
-
+'''
 ## for a single instances
 broker_url = 'amqp://guest:guest@rabbitmq:5672//'
 result_backend = 'redis://redis:6379/0'
-
 '''
+
 # For Multiple Instances
 broker_url = 'amqp://guest:guest@<BROKER_IP>:5672//'
 result_backend = 'redis://<REDIS_IP>:6379/0'
@@ -12,7 +12,7 @@ task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
 timezone = 'Asia/Dhaka'
-'''
+
 
 task_default_queue = 'celery_see'
 
